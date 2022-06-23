54e4d94e0aea4048e334291237bd0436b8757cb5
Ashish Kumar Burnwal <ashishburnwal293@gmail.com>
2022-06-23 18:33:12 +0530

    netinfo

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-import { AppState } from 'react-native';
+import { AppState } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+export const FOREGROUND = "APP_STATE.FOREGROUND";
+export const BACKGROUND = "APP_STATE.BACKGROUND";
+export const INACTIVE = "APP_STATE.INACTIVE";
+export const CONNECTED = "NETWORK_STATUS.CONNECTED";
+export const DISCONNECTED = "NETWORK_STATUS.DISCONNECTED";
 
-export const FOREGROUND = 'APP_STATE.FOREGROUND';
-export const BACKGROUND = 'APP_STATE.BACKGROUND';
-export const INACTIVE = 'APP_STATE.INACTIVE';
+export default () =>
+	(createStore) =>
+	(...args) => {
+		const store = createStore(...args);
 
-export default () => (createStore) => (...args) => {
-  const store = createStore(...args);
+		let currentState = false;
 
-  let currentState = '';
+		// const handleAppStateChange = (nextAppState) => {
+		// 	if (currentState !== nextAppState) {
+		// 		let type;
+		// 		if (nextAppState === "active") {
+		// 			type = FOREGROUND;
+		// 		} else if (nextAppState === "background") {
+		// 			type = BACKGROUND;
+		// 		} else if (nextAppState === "inactive") {
+		// 			type = INACTIVE;
+		// 		}
+		// 		if (type) {
+		// 			store.dispatch({
+		// 				type,
+		// 			});
+		// 		}
+		// 	}
+		// 	currentState = nextAppState;
+		// };
+		const handleNetInfoChange = (isConnected) => {
+			if (currentState !== isConnected) {
+				if (isConnected) {
+					store.dispatch({ type: "CONNECTED" });
+				} else {
+					store.dispatch({ type: "DISCONNECTED" });
+				}
+				currentState = isConnected;
+			}
+		};
+		NetInfo.addEventListener((state) => {
+			handleNetInfoChange(state.isConnected);
+		});
 
-  const handleAppStateChange = (nextAppState) => {
-    if (currentState !== nextAppState) {
-      let type;
-      if (nextAppState === 'active') {
-        type = FOREGROUND;
-      } else if (nextAppState === 'background') {
-        type = BACKGROUND;
-      } else if (nextAppState === 'inactive') {
-        type = INACTIVE;
-      }
-      if (type) {
-        store.dispatch({
-          type,
-        });
-      }
-    }
-    currentState = nextAppState;
-  };
+		// AppState.addEventListener("change", handleAppStateChange);
 
-  AppState.addEventListener('change', handleAppStateChange);
-  
-  // setTimeout to allow redux-saga to catch the initial state fired by redux-enhancer-react-native-appstate library
-  setTimeout(() => handleAppStateChange(AppState.currentState));
-  return store;
-};
+		// setTimeout to allow redux-saga to catch the initial state fired by redux-enhancer-react-native-appstate library
+		setTimeout(() => handleNetInfoChange(NetInfo.fetch().isConnected));
+		return store;
+	};
