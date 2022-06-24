63060749261ab688564890c37f47c910afe7a37b
Ashish Kumar Burnwal <ashishburnwal293@gmail.com>
2022-06-24 12:02:49 +0530

    added reducer

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import { AppState } from "react-native";
 import NetInfo from "@react-native-community/netinfo";
-export const FOREGROUND = "APP_STATE.FOREGROUND";
-export const BACKGROUND = "APP_STATE.BACKGROUND";
-export const INACTIVE = "APP_STATE.INACTIVE";
-export const CONNECTED = "NETWORK_STATUS.CONNECTED";
-export const DISCONNECTED = "NETWORK_STATUS.DISCONNECTED";
+const CONNECTED = "CONNECTED";
+const DISCONNECTED = "DISCONNECTED";
 
 export default () =>
 	(createStore) =>
@@ -12,25 +8,6 @@ export default () =>
 		const store = createStore(...args);
 
 		let currentState = false;
-
-		// const handleAppStateChange = (nextAppState) => {
-		// 	if (currentState !== nextAppState) {
-		// 		let type;
-		// 		if (nextAppState === "active") {
-		// 			type = FOREGROUND;
-		// 		} else if (nextAppState === "background") {
-		// 			type = BACKGROUND;
-		// 		} else if (nextAppState === "inactive") {
-		// 			type = INACTIVE;
-		// 		}
-		// 		if (type) {
-		// 			store.dispatch({
-		// 				type,
-		// 			});
-		// 		}
-		// 	}
-		// 	currentState = nextAppState;
-		// };
 		const handleNetInfoChange = (isConnected) => {
 			if (currentState !== isConnected) {
 				if (isConnected) {
@@ -45,9 +22,21 @@ export default () =>
 			handleNetInfoChange(state.isConnected);
 		});
 
-		// AppState.addEventListener("change", handleAppStateChange);
-
-		// setTimeout to allow redux-saga to catch the initial state fired by redux-enhancer-react-native-appstate library
+		// setTimeout to allow redux-saga to catch the initial state fired by redux-enhancer-react-native-netstate library
 		setTimeout(() => handleNetInfoChange(NetInfo.fetch().isConnected));
 		return store;
 	};
+
+export const initialState = {
+	isConnected: false,
+};
+export const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case CONNECTED:
+			return { ...state, isConnected: true };
+		case DISCONNECTED:
+			return { ...state, isConnected: false };
+		default:
+			return state;
+	}
+};
